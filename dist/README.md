6901ceb2619bbe57efde97362549127a9c3ad129
Jeremy Goldstein <JGoldstein@MLN.LOCAL>
2020-08-07 11:27:49 -0400

    dist version

---
+++ dist/README.md
@@ -0,0 +1,35 @@
+# Hold Pickup Location Batch Updater
+
+Script will batch update the pickup location for all holds with a status of "on hold" with a specified location code
+to a new pickup location code
+
+Two excel log files will be generated in the same directory as this script
+One that will include all holds prior to the update
+A second with any holds the script was unable to update
+
+Within the dist folder you will find a deployable for windows executable version of the script created using pyinstaller
+
+#Prerequistes
+
+In order to function you must complete the api_info.ini file in the same directory
+and add your cacert.pem file to the certifi folder
+
+#api_info.ini
+Requires valid credentials for both the Sierra holds API and sql access
+The file should be formatted like so
+
+[api]
+base_url = https://[local domain]/iii/sierra-api/v5
+client_key = [enter Sierra API key]
+client_secret = [enter Sierra API secret]
+sql_host = [enter host for Sierra SQL server]
+sql_user = [enter sql username]
+sql_pass = [enter sql password]
+
+#cacert.pem file
+To find where this file is located on your computer you may run the following python script
+
+import certifi
+certifi.where()
+
+Once you've located the file, copy it to the certifi folder
--- update_hold_pickuplocs.py
+++ update_hold_pickuplocs.py
@@ -1,15 +1,29 @@
-#Jeremy Goldstein
-#Minuteman Library Network
+'''
+Jeremy Goldstein
+Minuteman Library Network
+jgoldstein@minlib.net
 
-#Script to batch change the pickup location for all holds with a given pickup location
-#designed for cases where a location is forced to close and holds are made available at a neighboring location
-#Fails for cases where a patron has multiple holds on a single bib record
+In order to function you must complete the api_info.ini file in the same directory
+and add your cacert.pem file to the certifi folder per instructions in the readme
+
+Script will batch update the pickup location for all holds with a status of "on hold" with a specified location code
+to a new pickup location code
+
+Two excel log files will be generated in the same directory as this script
+One that will include all holds prior to the update
+A second with any holds the script was unable to update
+'''
 
 import requests
 import json
+import os
 import configparser
 from base64 import b64encode
 import psycopg2
+import xlsxwriter
+from datetime import date
+#used for pyintaller
+#from multiprocessing import Process, freeze_support
 
 def get_token():
     # config api    
@@ -29,6 +43,7 @@ def get_token():
     token = json_response["access_token"]
     return token
 
+#updates pickup location via sierra holds API
 def mod_hold(hold_id,is_frozen,new_location):
     config = configparser.ConfigParser()
     config.read('api_info.ini')
@@ -37,25 +52,68 @@ def mod_hold(hold_id,is_frozen,new_location):
     header = {"Authorization": "Bearer " + token, "Content-Type": "application/json;charset=UTF-8"}
     payload = {"pickupLocation" : new_location, "freeze": is_frozen}
     request = requests.put(url, data=json.dumps(payload), headers = header)
+
+#convert sql query results into formatted excel file
+def excelWriter(query_results,excelfile):
+    workbook = xlsxwriter.Workbook(excelfile,{'remove_timezone': True})
+    worksheet = workbook.add_worksheet()
+
+    #Formatting our Excel worksheet
+    worksheet.set_landscape()
+    worksheet.hide_gridlines(0)
+
+    #Formatting Cells
+    eformat= workbook.add_format({'text_wrap': True, 'valign': 'top', 'align': 'center'})
+    eformatlabel= workbook.add_format({'text_wrap': True, 'valign': 'top', 'bold': True, 'align': 'center'})
+
+    # Setting the column widths
+    worksheet.set_column(0,0,13.14)
+    worksheet.set_column(1,1,10.29)
+
+    #Inserting a header
+    worksheet.set_header('UpdatePickupLocations')
+
+    # Adding column labels
+    worksheet.write(0,0,'Hold ID', eformatlabel)
+    worksheet.write(0,1,'Is Frozen', eformatlabel)
+
+    # Writing the report for staff to the Excel worksheet
+    for rownum, row in enumerate(query_results):
+        worksheet.write(rownum+1,0,row[0], eformat)
+        worksheet.write(rownum+1,1,row[1], eformat)
+        
+    workbook.close()
     
 def main():
+	#certificate file needed for pyinstaller
+    os.environ['REQUESTS_CA_BUNDLE'] = "certifi/cacert.pem"
+    
+    #config for both API and SQL access
     config = configparser.ConfigParser()
     config.read('api_info.ini')
-    old_location = input("Enter location code to search, or type \'q\' to quit.\n")
-    new_location = input("enter location code you wish to change to.\n")
+    
+    #prompt user for pickup location codes to use, ignoring case
+    old_location = input("Enter pickup location code to change\nor type \'q\' to quit.\n\n").lower()
+    new_location = input("enter new pickup location code.\n").lower()
+    
+    
     while old_location != 'q':
-        if not old_location.endswith('z'):
+        #checks if the entered location codes are 4 characters long and end in a z
+        if not (old_location.endswith('z') and len(old_location) == 4):
             print('Invalid location code entered, please try again')
             break
-        if not new_location.endswith('z'):
+        if not (new_location.endswith('z')  and len(old_location) == 4):
             print('Invalid location code entered, please try again')
             break
-        confirm = input("This will change holds with location " + old_location + " to " + new_location + ".  Do you wish to proceed?  type \'y\' to continue.\n")
+        
+        #confirm before proceeding
+        confirm = input("This will change all holds with the pickup location " + old_location + " to " + new_location + ".  Do you wish to proceed?  type \'y\' to continue.\n").lower()
         if not confirm == 'y':
-            print('\nThis program will now quit.  Goodbye.')
+            #print('\nThis program will now quit.  Goodbye.')
             break
-        query = "select id, is_frozen from sierra_view.hold where pickup_location_code = '" + old_location
+        
         #Connecting to Sierra PostgreSQL database
+        query = "select h.id, h.is_frozen from sierra_view.hold h where h.pickup_location_code = '" + old_location + "' AND h.status = '0'"
         conn = psycopg2.connect("dbname='iii' user='" + config['api']['sql_user'] + "' host='" + config['api']['sql_host'] + "' port='1032' password='" + config['api']['sql_pass'] + "' sslmode='require'")
 
         #Opening a session and querying the database for weekly new items
@@ -64,16 +122,41 @@ def main():
         #For now, just storing the data in a variable. We'll use it later.
         rows = cursor.fetchall()
         conn.close()
+        
+        #printing to screen to show progress
+        print("\nGenerating log")
+        
+        #Generate log of holds the script is attempting to update
+        excelFile =  old_location+'UpdatePickupLoc{}.xlsx'.format(date.today())
+        excelWriter(rows,excelFile)
+        
+        #Iterate through results of SQL query to update each hold
+        print("\nUpdating locations")
         for hold_id, is_frozen in rows:
             print("hold id: " + str(hold_id))
             print("is_frozen: " + str(is_frozen))
             mod_hold(str(hold_id),is_frozen,new_location)
+        
+        #run query again to find holds that could not be updated
+        conn = psycopg2.connect("dbname='iii' user='" + config['api']['sql_user'] + "' host='" + config['api']['sql_host'] + "' port='1032' password='" + config['api']['sql_pass'] + "' sslmode='require'")
+
+        #Opening a session and querying the database for weekly new items
+        cursor = conn.cursor()
+        cursor.execute(query)
+        #For now, just storing the data in a variable. We'll use it later.
+        rows = cursor.fetchall()
+        conn.close()
+        
+        #Generate log of holds that could not be updated
+        excelFile2 =  old_location+'FailedToUpdate{}.xlsx'.format(date.today())
+        excelWriter(rows,excelFile2)
             
-        old_location = input("\nEnter another location code, or press \'q\' to quit.\n")
+        #Run again for a different location or quit
+        old_location = input("\nEnter another location, or type \'q\' to quit.\n").lower()
         
     print("\nThis program will now quit.  Goodbye.")
     
-   
-                    
-main()
+#used for executable created by pyinstaller
+if __name__ == '__main__':
+    main()
 
